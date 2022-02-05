81a347e0cbd2ce37c88e006f12d3ece3ffdeec85
Nicholas Antidormi <nicholas.antidormi@atoms.studio>
2022-02-05 14:25:20 +0100

    create some basic ui components

---
+++ components/Card.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="flex flex-col border border p-4 gap-y-3">
+		<slot name="header" />
+		<slot />
+	</div>
+</template>
---
+++ components/Grid.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="grid grid-cols-4 gap-6">
+		<slot />
+	</div>
+</template>
