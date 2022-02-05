9f061484c94509a484d9eba6915f164a0eb68107
Nicholas Antidormi <nicholas.antidormi@atoms.studio>
2022-02-05 13:17:05 +0100

    create some basic app components

---
+++ components/App/Image.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export type AppImageProps = {
+  src: string;
+  alt: string;
+  title: string;
+};
+</script>
+
+<script lang="ts" setup>
+defineProps<AppImageProps>();
+</script>
+
+<template>
+	<img :src="src" :alt="alt" :title="title" >
+</template>
---
+++ components/App/Link.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export type LinkProps = {
+  href: string;
+};
+</script>
+
+<script lang="ts" setup>
+defineProps<LinkProps>();
+</script>
+
+<template>
+	<a :href="href">
+		<slot />
+	</a>
+</template>
